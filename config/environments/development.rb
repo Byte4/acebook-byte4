0af5582f47e3fd5989e540062a17ac0c528037ae
antcin <ciniglioantonio@gmail.com>
2018-04-11 22:33:30 +0100

    install gem letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
