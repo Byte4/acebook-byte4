c1f4fdcbeb482384c1158e72685caa6f2fb95c34
antcin <ciniglioantonio@gmail.com>
2018-04-11 21:55:51 +0100

    write test for sending confirmation email to user after creation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '8db72df9b2c375b7828cfd14b5c0ce8b34f8c18806ff79cda407aca23d78b1e381427c103a664a76106747611d83d35aa472d54fc62d5644d09349b9e0eceedb'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -142,7 +142,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
