39cf026f251d6366ffa1c86cc21b610cb4c88334
durranee <durranee@hotmail.com>
2018-04-12 16:56:37 +0100

    all original tests passing

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
-    visit "/posts"
+
+  let(:user) {FactoryBot.create(:user)}
+
+  def fill_in_signin_fields
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'Log in'
+  end
+
+  scenario 'visiting the site to sign in' do
+    visit root_path
+    fill_in_signin_fields
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
 end
