2c7e58b4de6dacbe15a30d2ba5babb2ffec4db1f
Ajibaji <persiancookbook@gmail.com>
2018-04-12 17:31:07 +0100

    timezone and feature test for date display on posts PASSED

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'date'
 
 RSpec.feature "Timeline", type: :feature do
 
@@ -10,13 +11,22 @@ RSpec.feature "Timeline", type: :feature do
     click_button 'Log in'
   end
 
-  scenario 'visiting the site to sign in' do
+  scenario 'user can visit and post new post' do
     visit root_path
     fill_in_signin_fields
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+  end
+
+  scenario 'check to see if date appears on index page' do
+    visit root_path
+    fill_in_signin_fields
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content(Time.now.strftime("%d/%m/%Y at %I:%M%p"))
   end
 
 end
