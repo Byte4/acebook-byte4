415ad544d364d1e1130629ef1c24007dc6940768
durranee <durranee@hotmail.com>
2018-04-16 18:18:02 +0100

    likes and tests are done, user can only like once

--- spec/features/user_can_like_spec.rb
+++ spec/features/user_can_like_spec.rb
@@ -3,7 +3,8 @@ require 'date'
 
 RSpec.feature "Timeline", type: :feature do
 
-  let(:user) {FactoryBot.create(:user)}
+  let!(:user) {FactoryBot.create(:user)}
+  let!(:post) {FactoryBot.create(:post)}
 
   def fill_in_signin_fields
     fill_in 'user[email]', with: user.email
@@ -14,21 +15,8 @@ RSpec.feature "Timeline", type: :feature do
   scenario 'user can visit and like posts' do
     visit root_path
     fill_in_signin_fields
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-
-    click_link('#1')
-    expect(page).to have_content("1")
+    find("#like-#{post.id}").click
+    expect("#likes-counter-#{post.id}").to have_content("1")
   end
-  #
-  # scenario 'check to see if date appears on index page' do
-  #   visit root_path
-  #   fill_in_signin_fields
-  #   click_link "New post"
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
-  #   expect(page).to have_content(Time.now.strftime("%d/%m/%Y at %I:%M%p"))
-  # end
 
 end
