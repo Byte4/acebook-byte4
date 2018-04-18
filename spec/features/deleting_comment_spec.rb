ab53950373bec433e2316c155f821397694728f6
antcin <ciniglioantonio@gmail.com>
2018-04-18 11:23:02 +0100

    write test to for preventing user from deleting posts that do not belong to them

--- spec/features/deleting_comment_spec.rb
+++ spec/features/deleting_comment_spec.rb
@@ -5,7 +5,10 @@ feature "Deleting comments" do
     user = FactoryBot.create(:user)
     user_two = FactoryBot.create(:user, id: 2, email: 'hello@hello.com', first_name: "John", password: "abcdef" )
     post = FactoryBot.create(:post, user_id: user.id)
+
     comment = FactoryBot.create(:comment, user_id: user_two.id, post_id: post.id)
+
+    comment_two = FactoryBot.create(:comment, id: 2, post_id: post.id, content: 'This Acebook thingy is amazing')
     sign_in_with user_two
   end
 
@@ -13,7 +16,12 @@ feature "Deleting comments" do
     visit '/'
     expect(page).to have_content("test comment")
     click_link 'Delete'
-    save_and_open_page
     expect(page).to_not have_content("test comment")
+  end
+
+  scenario "user cannot delete posts that don't belong to them" do
+    visit '/'
+    expect(page).to have_content('This Acebook thingy is amazing')
+    expect(page).to_not have_css('Delete')
   end
 end
