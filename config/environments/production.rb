4ffa2c7ed0653c62b3e6f281f68918b5f1899572
durranee <durranee@hotmail.com>
2018-04-20 13:03:20 +0100

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.gem 'paperclip'
   Paperclip.options[:command_path] = "/usr/local/bin/"
 end
