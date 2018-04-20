93a6c4ebdcb2900d49d3a3b294bcf440f06e9a52
agnesdonat <agi.donat@gmail.com>
2018-04-20 10:09:40 +0100

    Tested picture showing on posts and comments

--- spec/features/user_can_dislike_spec.rb
+++ spec/features/user_can_dislike_spec.rb
@@ -11,8 +11,6 @@ RSpec.feature "Dislike", type: :feature do
     sign_in_with(user)
     find("#dislike-#{post.id}").click
     expect(page).to have_css("span#dislikes-counter-#{post.id}", text: '1')
-
-    # expect("#dislikes-counter-#{post.id}").to have_content("1")
   end
 
   scenario 'clicking twice on dislike will undo the dislike' do
@@ -21,8 +19,6 @@ RSpec.feature "Dislike", type: :feature do
     find("#dislike-#{post.id}").click
     find("#dislike-#{post.id}").click
     expect(page).not_to have_css("span#dislikes-counter-#{post.id}", text: '1')
-
-    # expect("#dislikes-counter-#{post.id}").not_to have_content("1")
   end
 
 end
